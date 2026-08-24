c6afdf2bdad105a58adb30004ff11f6c5c79b65e
Trlso <110932622+Trlso@users.noreply.github.com>
2026-08-24 22:22:07 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,5 +5,5 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 sed -i '1i src-git qmodem https://github.com/FUjr/QModem' feeds.conf.default
 sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf.default
 
-sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '4i src-git small https://github.com/kenzok8/small' feeds.conf.default
