05f4ce5e73a0d039048dd87bb56739ae4601c8a6
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-03 14:49:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,3 +7,7 @@ sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf
 
 sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '4i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
+./scripts/feeds install -a
