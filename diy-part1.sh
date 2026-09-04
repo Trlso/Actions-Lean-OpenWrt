85437b475c009543b25890685791c97031c96a10
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-04 13:19:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,5 +7,3 @@ sed -i '1i src-git modem https://github.com/fildunsky/luci-app-5gmodem.git' feed
 sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' feeds.conf.default
 
 sed -i '3i src-git smpackage https://github.com/kenzok8/small-package.git' feeds.conf.default
-
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
