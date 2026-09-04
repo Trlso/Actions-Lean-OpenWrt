5df3e9d4feb8e2c0e7286ef4ae7b984f287eda77
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-04 11:50:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,9 +5,4 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 sed -i '1i src-git qmodem https://github.com/FUjr/QModem' feeds.conf.default
 sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf.default
 
-sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '4i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
-./scripts/feeds install -a
+sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
