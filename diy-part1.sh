b1b90c15a32340faf82057083e8177884b1a2e0a
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-04 12:59:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 sed -i '1i src-git modem https://github.com/fildunsky/luci-app-5gmodem.git' feeds.conf.default
+
 sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' feeds.conf.default
 
 sed -i '3i src-git smpackage https://github.com/kenzok8/small-package.git' feeds.conf.default
