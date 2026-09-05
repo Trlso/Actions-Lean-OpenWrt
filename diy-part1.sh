01a05151ca4e21ee19cbdd7b9910398902383719
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-05 21:52:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,8 +2,8 @@
 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
-sed -i '1i src-git modem https://github.com/fildunsky/luci-app-5gmodem.git' feeds.conf.default
+sed -i '1i src-git qmodem https://github.com/FUjr/QModem' feeds.conf.default
 
-sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' feeds.conf.default
+sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf.default
 
-sed -i '3i src-git smpackage https://github.com/kenzok8/small-package.git' feeds.conf.default
+sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
