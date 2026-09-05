fde3fcf349eba68e2701fa04c4c042575829c5b3
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-05 21:55:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,8 +2,8 @@
 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
-sed -i '1i src-git qmodem https://github.com/FUjr/QModem' feeds.conf.default
+sed -i '1i src-git qmodem https://github.com/FUjr/QModem.git' feeds.conf.default
 
-sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf.default
+sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol.git' feeds.conf.default
 
-sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
