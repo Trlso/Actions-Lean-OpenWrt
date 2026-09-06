90d463112a0734af41f3cb2d75eb97cb10e169f0
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-06 13:07:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,4 @@ sed -i '1i src-git qmodem https://github.com/FUjr/QModem' feeds.conf.default
 
 sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf.default
 
-sed -i '3i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
