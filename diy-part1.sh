1e45dbae37a608b2f831360b684bc4a9e761b59b
Trlso <110932622+Trlso@users.noreply.github.com>
2026-09-06 21:31:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,5 +7,3 @@ sed -i '1i src-git qmodem https://github.com/FUjr/QModem' feeds.conf.default
 sed -i '2i src-git fancontrol https://github.com/JiaY-shi/fancontrol' feeds.conf.default
 
 sed -i '3i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
-rm -rf feeds/helloworld/gn 2>/dev/null || true
